360f1aee275701098d76696b5d23e6b12d767a15
systia <syscode@skiff.com>
2024-02-13 23:50:25 +0100

    feat(mini): add some modules

--- config/plugins/mini.nix
+++ config/plugins/mini.nix
@@ -3,6 +3,41 @@
     enable = true;
     modules = {
       indentscope = { };
+      move = { };
+      trailspace = { };
+      cursorword = { };
+      hipatterns = {
+        highlighters = {
+          fixme = {
+            pattern = "%f[%w]()FIXME()%f[%W]";
+            group = "MiniHipatternsFixme";
+          };
+          hack = {
+            pattern = "%f[%w]()HACK()%f[%W]";
+            group = "MiniHipatternsHack";
+          };
+          todo = {
+            pattern = "%f[%w]()TODO()%f[%W]";
+            group = "MiniHipatternsTodo";
+          };
+          note = {
+            pattern = "%f[%w]()NOTE()%f[%W]";
+            group = "MiniHipatternsNote";
+          };
+          hex_color = "hipatterns.gen_highlighter.hex_color()";
+        };
+      };
+    };
+  };
+
+  highlightOverride = {
+    MiniCursorword = {
+      underline = true;
+      bg = "NONE";
+    };
+    MiniCursorwordCurrent = {
+      underline = true;
+      bg = "NONE";
     };
   };
 }

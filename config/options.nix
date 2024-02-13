eea4957bbf774d4b85d3d4f0c9d6b5bd5ae5f422
systia <syscode@skiff.com>
2024-02-13 20:26:28 +0100

    feat(config): add options

---
+++ config/options.nix
@@ -0,0 +1,42 @@
+{
+  config = {
+    options = {
+      number = true;
+      relativenumber = true;
+
+      tabstop = 4;
+      softtabstop = 4;
+      expandtab = true;
+      smarttab = true;
+
+      shiftwidth = 4;
+      smartindent = true;
+
+      breakindent = true;
+
+      wrap = true;
+
+      ignorecase = true;
+      smartcase = true;
+
+      completeopt = [
+        "menuone"
+        "preview"
+        "noinsert"
+        "noselect"
+      ];
+
+      swapfile = false;
+      backup = false;
+      undofile = true;
+
+      cursorline = true;
+
+      scrolloff = 8;
+
+      timeoutlen = 300;
+
+      showmode = false;
+    };
+  };
+}

0a694b8651b86639bfe94811608c839a7a5b3711
systia <syscode@skiff.com>
2024-03-16 18:15:18 +0100

    refactor: update flake

--- config/plugins/nvim-cmp.nix
+++ config/plugins/nvim-cmp.nix
@@ -1,36 +1,26 @@
 {
-  plugins.nvim-cmp = {
+  plugins.cmp = {
     enable = true;
-    sources = [
-      { name = "nvim_lsp"; }
-      { name = "luasnip"; }
-      { name = "buffer"; }
-    ];
-    autoEnableSources = true;
-    window = {
-      completion.border = "rounded";
-      documentation.border = "rounded";
-    };
-    snippet.expand = "luasnip";
-    mapping = {
-      "<C-Space>" = "cmp.mapping.complete()";
-      "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-      "<C-e>" = "cmp.mapping.close()";
-      "<C-f>" = "cmp.mapping.scroll_docs(4)";
-      "<CR>" = "cmp.mapping.confirm({ select = true })";
-      "<S-Tab>" = {
-        action = "cmp.mapping.select_prev_item()";
-        modes = [
-          "i"
-          "s"
-        ];
+    settings = {
+      sources = [
+        { name = "nvim_lsp"; }
+        { name = "luasnip"; }
+        { name = "buffer"; }
+      ];
+      autoEnableSources = true;
+      window = {
+        completion.border = "rounded";
+        documentation.border = "rounded";
       };
-      "<Tab>" = {
-        action = "cmp.mapping.select_next_item()";
-        modes = [
-          "i"
-          "s"
-        ];
+      snippet.expand = "luasnip";
+      mapping = {
+        "<C-Space>" = "cmp.mapping.complete()";
+        "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+        "<C-e>" = "cmp.mapping.close()";
+        "<C-f>" = "cmp.mapping.scroll_docs(4)";
+        "<CR>" = "cmp.mapping.confirm({ select = true })";
+        "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+        "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
       };
     };
   };
